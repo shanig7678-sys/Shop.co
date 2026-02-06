9d06f953f7d492cd97c3a4829589559e5dc33821
unknown <shanig7678@gmail.com>
2026-02-05 17:47:17 -0800

    CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,17 +23,17 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer-content">
-                <div className='column'>
+                <div className='logo-column'>
                     <img src='./logo.svg' alt='logo' />
                     <p>
                         We have clothes that suits your style and
                         which you’re proud to wear. From women to men.
                     </p>
-                    <div>
+                    <div className='social-media-icon'>
                         <div className='circle' id='twitter'>
                             <img src='./twitter.svg' alt='twitter' />
                         </div>
-                        <div className='circle' id='facbook'>
+                        <div className='circle' id='facebook'>
                             <img src='./facebook.svg' alt='facebook' />
                         </div>
                         <div className='circle' id='instagram'>
@@ -44,6 +44,28 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
+                <div className='column'>
+                    <h1>Company</h1>
+                    <p>About</p>
+                    <p>Features</p>
+                    <p>Works</p>
+                    <p>Career</p>
+                </div>
+                <div className="column">
+                    <h1>Help</h1>
+                    <p>Customer Support</p>
+                    <p>Delivery Details</p>
+                    <p>Terms & Conditions</p>
+                    <p>Privacy Policy</p>
+                </div>
+                <div className="column">
+                    <h1>FAQ</h1>
+                    <p>Account</p>
+                    <p>Manange Delivers</p>
+                    <p>Orders</p>
+                    <p>Payments</p>
+                </div>
+                
             </div>
         </footer>
 
