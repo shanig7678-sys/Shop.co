c1534ce7febc8f969c53f25dcac2d764bad35b91
unknown <shanig7678@gmail.com>
2026-02-05 20:43:14 -0800

    Completed Landing Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,50 +22,91 @@ const Footer = () => {
                     </button>
                 </div>
             </div>
-            <div className="footer-content">
-                <div className='logo-column'>
-                    <img src='./logo.svg' alt='logo' />
-                    <p>
-                        We have clothes that suits your style and
-                        which you’re proud to wear. From women to men.
-                    </p>
-                    <div className='social-media-icon'>
-                        <div className='circle' id='twitter'>
-                            <img src='./twitter.svg' alt='twitter' />
+            <div className="main-footer">
+
+
+                <div className="footer-content">
+                    <div className='logo-column'>
+                        <img src='./logo.svg' alt='logo' />
+                        <p>
+                            We have clothes that suits your style and
+                            which you’re proud to wear. From women to men.
+                        </p>
+                        <div className='social-media-icon'>
+                            <div className='circle' id='twitter'>
+                                <img src='./twitter.svg' alt='twitter' />
+                            </div>
+                            <div className='circle' id='facebook'>
+                                <img src='./facebook.svg' alt='facebook' />
+                            </div>
+                            <div className='circle' id='instagram'>
+                                <img src='./instagram.svg' alt='instagram' />
+                            </div>
+                            <div className='circle' id='github'>
+                                <img src='./github.svg' alt='github' />
+                            </div>
                         </div>
-                        <div className='circle' id='facebook'>
-                            <img src='./facebook.svg' alt='facebook' />
+                    </div>
+                    <div className='column'>
+                        <h1>Company</h1>
+                        <div className="column-paragraph">
+                            <p>About</p>
+                            <p>Features</p>
+                            <p>Works</p>
+                            <p>Career</p>
                         </div>
-                        <div className='circle' id='instagram'>
-                            <img src='./instagram.svg' alt='instagram' />
+                    </div>
+                    <div className="column">
+                        <h1>Help</h1>
+                        <div className="column-paragraph">
+                            <p>Customer Support</p>
+                            <p>Delivery Details</p>
+                            <p>Terms & Conditions</p>
+                            <p>Privacy Policy</p>
                         </div>
-                        <div className='circle' id='github'>
-                            <img src='./github.svg' alt='github' />
+
+                    </div>
+                    <div className="column">
+                        <h1>FAQ</h1>
+                        <div className="column-paragraph">
+                            <p>Account</p>
+                            <p>Manange Delivers</p>
+                            <p>Orders</p>
+                            <p>Payments</p>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <h1>RESOURCES</h1>
+                        <div className="column-paragraph">
+                            <p>Free eBooks</p>
+                            <p>Development Tutorials</p>
+                            <p>How to - Blog</p>
+                            <p>Youtube Playlist</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="logo-vertical-footer">
+                    <p>Shop.co © 2000-2023, All Rights Reserved</p>
+                    <div className="footer-card">
+                        <div className="card">
+                            <img src="./G-Pay.svg" alt="gpay" />
+                        </div>
+                        <div className="card">
+                            <img src="./Mastercard.svg" alt="svg" />
+                        </div>
+                        <div className="card">
+                            <img src="./Paypal.svg" alt="paypal" />
+                        </div>
+                        <div className="card">
+                            <img src="Visa.svg" alt="visa" />
+                        </div>
+                        <div className="card">
+                            <img src="./Pay.svg" alt="pay" />
                         </div>
                     </div>
                 </div>
-                <div className='column'>
-                    <h1>Company</h1>
-                    <p>About</p>
-                    <p>Features</p>
-                    <p>Works</p>
-                    <p>Career</p>
-                </div>
-                <div className="column">
-                    <h1>Help</h1>
-                    <p>Customer Support</p>
-                    <p>Delivery Details</p>
-                    <p>Terms & Conditions</p>
-                    <p>Privacy Policy</p>
-                </div>
-                <div className="column">
-                    <h1>FAQ</h1>
-                    <p>Account</p>
-                    <p>Manange Delivers</p>
-                    <p>Orders</p>
-                    <p>Payments</p>
-                </div>
-                
             </div>
         </footer>
 
